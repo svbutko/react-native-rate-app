df82a997243768431eeef6a585ba71ed9feddae2
svbutko <svbutko@gmail.com>
2025-06-10 16:16:17 +0200

    Add MainActor to fix errors when using Swift 6.1

--- ios/RateApp.swift
+++ ios/RateApp.swift
@@ -3,16 +3,16 @@ import StoreKit
 
 @objc(RateApp)
 class RateApp: NSObject {
-  
+
   private let noActiveSceneError = "no_active_scene"
   private let unsupportedPlatformError = "unsupported_platform"
-  
+
   @objc
   static func requiresMainQueueSetup() -> Bool {
     return false
   }
-  
-  @objc
+
+  @MainActor @objc
   func requestReview(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
     if let scene = findActiveScene() {
       if #available(iOS 16.0, *) {
@@ -31,20 +31,20 @@ class RateApp: NSObject {
       reject(noActiveSceneError, "No active scene found", nil)
     }
   }
-  
+
   @objc
   func requestReviewAppGallery(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
     reject(unsupportedPlatformError, "App Gallery reviews are not supported on iOS", nil)
   }
-  
+
   @objc
   func requestReviewGalaxyStore(_ packageName: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
     reject(unsupportedPlatformError, "Galaxy Store reviews are not supported on iOS", nil)
   }
-  
-  private func findActiveScene() -> UIWindowScene? {
+
+    @MainActor private func findActiveScene() -> UIWindowScene? {
     for scene in UIApplication.shared.connectedScenes {
-      if let windowScene = scene as? UIWindowScene, 
+      if let windowScene = scene as? UIWindowScene,
          windowScene.activationState == .foregroundActive {
         return windowScene
       }
