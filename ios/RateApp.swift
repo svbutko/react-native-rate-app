16c3cf99d49dbf5246f512c0831115399197be7e
svbutko <svbutko@gmail.com>
2025-06-19 10:53:21 +0200

    Adjust requestReview to support concurrency changes made in  Swift 5.5

--- ios/RateApp.swift
+++ ios/RateApp.swift
@@ -3,52 +3,46 @@ import StoreKit
 
 @objc(RateApp)
 class RateApp: NSObject {
-  
+
   private let noActiveSceneError = "no_active_scene"
   private let unsupportedPlatformError = "unsupported_platform"
-  
+
   @objc
   static func requiresMainQueueSetup() -> Bool {
     return false
   }
-  
+
   @objc
   func requestReview(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-    if let scene = findActiveScene() {
-      if #available(iOS 16.0, *) {
-        Task { @MainActor in
-          AppStore.requestReview(in: scene)
-        }
-      } else {
-        if #available(iOS 14.0, *) {
-          SKStoreReviewController.requestReview(in: scene)
-        } else {
-          SKStoreReviewController.requestReview()
+      var sceneExists = false
+
+      Task { @MainActor in
+        if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+            sceneExists = true
+              if #available(iOS 16.0, *) {
+                  AppStore.requestReview(in: scene)
+              } else if #available(iOS 14.0, *) {
+                  SKStoreReviewController.requestReview(in: scene)
+              } else {
+                  SKStoreReviewController.requestReview()
+              }
         }
       }
-      resolve(true)
-    } else {
-      reject(noActiveSceneError, "No active scene found", nil)
-    }
+
+      if (sceneExists) {
+          resolve(true)
+      } else {
+          reject(noActiveSceneError, "No active scene found", nil)
+      }
   }
-  
+
   @objc
   func requestReviewAppGallery(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
     reject(unsupportedPlatformError, "App Gallery reviews are not supported on iOS", nil)
   }
-  
+
   @objc
   func requestReviewGalaxyStore(_ packageName: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
     reject(unsupportedPlatformError, "Galaxy Store reviews are not supported on iOS", nil)
-  }
-  
-  private func findActiveScene() -> UIWindowScene? {
-    for scene in UIApplication.shared.connectedScenes {
-      if let windowScene = scene as? UIWindowScene, 
-         windowScene.activationState == .foregroundActive {
-        return windowScene
-      }
-    }
-    return nil
   }
 }
